7bdff2a22b3787a79eee17962214302b007f31ca
music <75509151@qq.com>
2017-02-12 15:11:41 +0800

    add sqlite run normal in multi thread

--- sql3/first/sql2.py
+++ sql3/first/sql2.py
@@ -13,6 +13,30 @@ c = conn.cursor()
 # conn.commit()
 
 
+def update_normal(information):
+    updates = ", ".join(["`" + field + "`" + '=:' + field for field in information.keys() if field != 'name'])
+    where = ' WHERE name == :name'
+    values = information
+    query = 'UPDATE firms SET ' + updates + where
+    print query
+    conn = sqlite3.connect('test.db', isolation_level="IMMEDIATE", timeout=60, check_same_thread=False)
+    c = conn.cursor()
+    print "exec"
+
+    try:
+        c.execute(query, values)
+        time.sleep(3)
+
+        # raise TypeError, "HEHE"
+        conn.commit()
+    except:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
+    print "commit"
+
+
 def update(information):
     # print information
     # for filed in information.keys():
@@ -36,8 +60,11 @@ def update(information):
         conn.commit()
     except:
         conn.rollback()
+        raise
     finally:
+
         lock.release()
+
     print "commit"
 
 
@@ -121,10 +148,11 @@ for i in range(10):
     # thread.start_new_thread(update, (dict(name='bar', founded='1062', rev='7 MILLION DOLLARS!'), ))
     # thread.start_new_thread(update_f1, (dict(name='bar', founded='1062', rev='7 MILLION DOLLARS!'), ))
     # thread.start_new_thread(update_f2, (dict(name='bar', founded='1062', rev='7 MILLION DOLLARS!'), ))
-    thread.start_new_thread(update_f3, (dict(name='bar', founded='1062', rev='7 MILLION DOLLARS!'), ))
+    thread.start_new_thread(update_normal, (dict(name='bar', founded='1062', rev='7 MILLION DOLLARS!'), ))
 
 update(dict(name='bar', founded='1062', rev='7 MILLION DOLLARS!'))
 # update({})
 print c.execute('select * from firms').fetchall()
 
+conn.close()
 time.sleep(1000)
