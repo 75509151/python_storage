e3e49ebb0f471471d6a20db729aadec2487b092e
music <75509151@qq.com>
2017-05-22 20:02:40 +0800

    ex_roi_get_line

--- open_cv/img/ex_roi_get_line.py
+++ open_cv/img/ex_roi_get_line.py
@@ -44,49 +44,62 @@ class EdgePanel(wx.Panel):
         self.bmp = wx.BitmapFromBuffer(width, height, self.rbg_img)
 
         self.edge_img = wx.StaticBitmap(self, size=(height, width))
-
+        print "origin", width, height
         self.edge_img.SetBitmap(self.bmp)
-        # self.Bind(wx.EVT_PAINT, self.OnPaint)
-        # self.Bind(wx.EVT_TIMER, self.NextFrame)
+        self.sp = 5
+        self.sr = 5
+        self.w = width
+        self.h = height
 
-        # self.Refresh()
+    def adjust_sp(self, sp):
+        self.sp = sp
+        self.fliter_img()
 
-    def OnPaint(self, evt):
-        dc = wx.BufferedPaintDC(self)
-        print "paint", self.bmp
-        dc.DrawBitmap(self.bmp, 0, 0)
+    def adjust_sr(self, sr):
+        self.sr = sr
+        self.fliter_img()
 
-    def NextFrame(self, event):
-        self.Refresh()
-
-    def detect(self, sp, sr):
-        image = cv2.pyrMeanShiftFiltering(self.origin_img, sp, sr)
+    def fliter_img(self):
+        image = cv2.pyrMeanShiftFiltering(self.origin_img, self.sp, self.sr)
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        kernel_size = 3
         detected_edges = cv2.Canny(gray_img, 20, 150, apertureSize=kernel_size)
         dst = cv2.bitwise_and(self.origin_img, self.origin_img, mask=detected_edges)
+        self.rbg_img = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+        self.bmp = wx.BitmapFromBuffer(self.w, self.h, self.rbg_img)
+        self.edge_img.SetBitmap(self.bmp)
 
 
 class CtrlPanel(wx.Panel):
     def __init__(self, parent):
-        wx.Panel.__init__(self, parent)
+        wx.Panel.__init__(self, parent, size=(400, 150))
+        self.parent = parent
         vbox = wx.BoxSizer(wx.VERTICAL)
 
-        self.sld = wx.Slider(self, value=10, minValue=1, maxValue=100,
-                             style=wx.SL_HORIZONTAL | wx.SL_LABELS)
+        self.sp_sld = wx.Slider(self, value=5, minValue=1, maxValue=100,
+                                size=(400, 50), style=wx.SL_HORIZONTAL | wx.SL_LABELS)
 
-        vbox.Add(self.sld, 1, flag=wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL | wx.TOP, border=20)
-        self.sld.Bind(wx.EVT_SLIDER, self.OnSliderScroll)
-        self.txt = wx.StaticText(self, label='Yiibai.com', style=wx.ALIGN_CENTER)
-        vbox.Add(self.txt, 1, wx.ALIGN_CENTRE_HORIZONTAL)
+        vbox.Add(self.sp_sld, 1, flag=wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL | wx.TOP)
+        self.sp_sld.Bind(wx.EVT_SLIDER, self.slider_sp)
+        self.sr_sld = wx.Slider(self, value=5, minValue=1, maxValue=100,
+                                size=(400, 50), style=wx.SL_HORIZONTAL | wx.SL_LABELS)
 
+        vbox.Add(self.sr_sld, 1, flag=wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL | wx.TOP)
+
+        self.sr_sld.Bind(wx.EVT_SLIDER, self.slider_sr)
         self.SetSizer(vbox)
 
-    def OnSliderScroll(self, e):
+    def slider_sp(self, e):
         obj = e.GetEventObject()
         val = obj.GetValue()
-        font = self.GetFont()
-        font.SetPointSize(self.sld.GetValue())
-        self.txt.SetFont(font)
+        print val
+        self.parent.edge_pnl.adjust_sp(val)
+
+    def slider_sr(self, e):
+        obj = e.GetEventObject()
+        val = obj.GetValue()
+        print val
+        self.parent.edge_pnl.adjust_sr(val)
 
 
 class Mywin(wx.Frame):
@@ -98,13 +111,13 @@ class Mywin(wx.Frame):
     def InitUI(self):
         v_lay = wx.BoxSizer(wx.VERTICAL)
         img_path = path + 'ex_ocupity_lg.jpg'
-        ctrl_pnl = CtrlPanel(self)
-        img_pnl = OriginImgPanel(self, img_path)
-        edge_pnl = EdgePanel(self, img_path)
+        self.ctrl_pnl = CtrlPanel(self)
+        self.img_pnl = OriginImgPanel(self, img_path)
+        self.edge_pnl = EdgePanel(self, img_path)
 
-        v_lay.Add(ctrl_pnl)
-        v_lay.Add(img_pnl)
-        v_lay.Add(edge_pnl)
+        v_lay.Add(self.ctrl_pnl)
+        v_lay.Add(self.img_pnl)
+        v_lay.Add(self.edge_pnl)
         self.SetSizer(v_lay)
         self.SetSize((500, 800))
         self.Centre()
