3e636f7467996ff96f6ab1f65f5c716b317dcc9a
music <75509151@qq.com>
2017-05-23 11:15:32 +0800

    add threshold

--- open_cv/img/ex_roi_get_line.py
+++ open_cv/img/ex_roi_get_line.py
@@ -10,6 +10,7 @@ path = "/home/mm/code_dept/python/resource/img/"
 
 
 class OriginImgPanel(wx.Panel):
+
     def __init__(self, parent, img_path):
         self.background = wx.Image(img_path, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         wx.Panel.__init__(self, parent=parent, size=(
@@ -35,21 +36,34 @@ class OriginImgPanel(wx.Panel):
 
 
 class EdgePanel(wx.Panel):
+
     def __init__(self, parent, img_path):
         wx.Panel.__init__(self, parent)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+
         self.origin_img = cv2.imread(img_path)
         height, width = self.origin_img.shape[:2]
-        self.SetSize((height, width))
+        self.SetSize((height * 3, width * 3))
         self.rbg_img = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2RGB)
         self.bmp = wx.BitmapFromBuffer(width, height, self.rbg_img)
 
-        self.edge_img = wx.StaticBitmap(self, size=(height, width))
-        print "origin", width, height
+        self.edge_img = wx.StaticBitmap(self, size=(width, height))
+
         self.edge_img.SetBitmap(self.bmp)
+
+        hbox.Add(self.edge_img, 1)
+        hbox.Add((-1, -1), 1)
+        self.binary_img = wx.StaticBitmap(self, size=(width, height))
+
+        self.binary_img.SetBitmap(self.bmp)
+        hbox.Add(self.binary_img)
         self.sp = 5
         self.sr = 5
+        self.threshold = 20
         self.w = width
         self.h = height
+        self.cv_bmp = None
+        self.SetSizer(hbox)
 
     def adjust_sp(self, sp):
         self.sp = sp
@@ -59,40 +73,63 @@ class EdgePanel(wx.Panel):
         self.sr = sr
         self.fliter_img()
 
+    def adjust_threshold(self, th):
+        self.threshold = th
+        self.threshold_img()
+
     def fliter_img(self):
         image = cv2.pyrMeanShiftFiltering(self.origin_img, self.sp, self.sr)
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         kernel_size = 3
         detected_edges = cv2.Canny(gray_img, 20, 150, apertureSize=kernel_size)
-        dst = cv2.bitwise_and(self.origin_img, self.origin_img, mask=detected_edges)
-        self.rbg_img = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+        self.cv_bmp = cv2.bitwise_and(self.origin_img, self.origin_img, mask=detected_edges)
+        self.rbg_img = cv2.cvtColor(self.cv_bmp, cv2.COLOR_BGR2RGB)
         self.bmp = wx.BitmapFromBuffer(self.w, self.h, self.rbg_img)
         self.edge_img.SetBitmap(self.bmp)
 
+    def threshold_img(self):
+        if self.cv_bmp is not None:
+            gray_img = cv2.cvtColor(self.cv_bmp, cv2.COLOR_BGR2GRAY)
+            ret, mask = cv2.threshold(gray_img, self.threshold, 255, cv2.THRESH_BINARY)
+            # mask_inv = cv2.bitwise_not(mask)
+            img = cv2.bitwise_and(self.cv_bmp, self.cv_bmp, mask=mask)
+            bmp = wx.BitmapFromBuffer(self.w, self.h, img)
+            self.binary_img.SetBitmap(bmp)
+
 
 class CtrlPanel(wx.Panel):
+
     def __init__(self, parent):
-        wx.Panel.__init__(self, parent, size=(400, 150))
+        wx.Panel.__init__(self, parent, size=(420, 150))
         self.parent = parent
-        vbox = wx.BoxSizer(wx.VERTICAL)
+        # vbox = wx.BoxSizer(wx.VERTICAL)
+        sizer = wx.GridBagSizer(hgap=10, vgap=0)
 
-        self.sp_sld = wx.Slider(self, value=5, minValue=1, maxValue=100,
-                                size=(400, 50), style=wx.SL_HORIZONTAL | wx.SL_LABELS)
+        self.sp_sld = wx.Slider(self, value=5, minValue=1, maxValue=40,
+                                size=(200, 50), style=wx.SL_HORIZONTAL | wx.SL_LABELS)
 
-        vbox.Add(self.sp_sld, 1, flag=wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL | wx.TOP)
+        # vbox.Add(self.sp_sld)
+        sizer.Add(self.sp_sld, (0, 0))
         self.sp_sld.Bind(wx.EVT_SLIDER, self.slider_sp)
-        self.sr_sld = wx.Slider(self, value=5, minValue=1, maxValue=100,
-                                size=(400, 50), style=wx.SL_HORIZONTAL | wx.SL_LABELS)
+        self.sr_sld = wx.Slider(self, value=5, minValue=1, maxValue=40,
+                                size=(200, 50), style=wx.SL_HORIZONTAL | wx.SL_LABELS)
 
-        vbox.Add(self.sr_sld, 1, flag=wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL | wx.TOP)
-
+        # vbox.Add(self.sr_sld)
+        sizer.Add(self.sr_sld, (0, 1))
         self.sr_sld.Bind(wx.EVT_SLIDER, self.slider_sr)
-        self.SetSizer(vbox)
+
+        self.thre_sld = wx.Slider(self, value=5, minValue=1, maxValue=40,
+                                  size=(200, 50), style=wx.SL_HORIZONTAL | wx.SL_LABELS)
+
+        sizer.Add(self.thre_sld, (1, 1))
+        self.thre_sld.Bind(wx.EVT_SLIDER, self.slider_threshold)
+
+        self.SetSizer(sizer)
 
     def slider_sp(self, e):
         obj = e.GetEventObject()
         val = obj.GetValue()
-        print val
+        print "sp", val
         self.parent.edge_pnl.adjust_sp(val)
 
     def slider_sr(self, e):
@@ -100,6 +137,17 @@ class CtrlPanel(wx.Panel):
         val = obj.GetValue()
         print val
         self.parent.edge_pnl.adjust_sr(val)
+
+    def slider_canny(self, e):
+        obj = e.GetEventObject()
+        val = obj.GetValue()
+        print val
+
+    def slider_threshold(self, e):
+        obj = e.GetEventObject()
+        val = obj.GetValue()
+        print val
+        self.parent.edge_pnl.adjust_threshold(val)
 
 
 class Mywin(wx.Frame):
@@ -119,7 +167,7 @@ class Mywin(wx.Frame):
         v_lay.Add(self.img_pnl)
         v_lay.Add(self.edge_pnl)
         self.SetSizer(v_lay)
-        self.SetSize((500, 800))
+        self.SetSize((1000, 900))
         self.Centre()
         self.Show(True)
 
